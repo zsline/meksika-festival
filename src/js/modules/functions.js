01e0114a0b87f9fe46b86bd19b8bb6aade9a2309
zsline <sergei.dereka@gmail.com>
2023-03-06 21:35:35 +0200

    sliders

--- src/js/modules/functions.js
+++ src/js/modules/functions.js
@@ -1,3 +1,5 @@
+import Swiper, { Navigation, Pagination } from "swiper";
+
 /* Проверка поддержки webp, добавление класса для html */
 export function isWebp() {
     //проверка поддержки webp
@@ -22,3 +24,34 @@ if (document.querySelector('.nav-menu')) {
         menu.classList.toggle('nav-menu--active');
     })
 }
+
+const swiper = new Swiper('.first-fest__slider, .second-fest__slider', {
+    modules: [Navigation, Pagination],
+    slidesPerView: 1,
+    spaceBetween: 10,
+    loop: true,
+
+    navigation: {
+        nextEl: '.fest__slider-next',
+        prevEl: '.fest__slider-prev',
+    },
+    autoplay: {
+        delay: 3000,
+    },
+});
+const swiper2 = new Swiper('.second-fest__slider', {
+    modules: [Navigation, Pagination],
+    slidesPerView: 1,
+    spaceBetween: 10,
+    loop: true,
+
+    navigation: {
+        nextEl: '.second__slider-next',
+        prevEl: '.second__slider-prev',
+    },
+    autoplay: {
+        delay: 3000,
+    },
+});
+
+// const swiper = new Swiper('.first-fest__slider');
